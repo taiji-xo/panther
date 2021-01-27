1201854877f9f5cc8f79fbcbf5fbbb662690c268
Tate Hanawalt <tate.hanawalt@runpanther.io>
2021-01-27 13:45:44 -0600

    fix: replacing unintentionally merged files in branch back to the current master branch version of the file

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -68,10 +68,6 @@ export const DEFAULT_DEDUP_FUNCTION =
 export const DEFAULT_ALERT_CONTEXT_FUNCTION =
   "# def alert_context(event):\n\t#  (Optional) Return a dictionary with additional data to be included in the alert sent to the SNS/SQS/Webhook destination\n\t# return {'key':'value'}";
 
-// PSA - Backend validations are in place to verify object resource types. Validations are tested
-// against hardcoded sets of valid resource types. Modifications to this set might need to be
-// repeated in the backend sets and vice versa.
-// Those sets are currently located in the snapshot_poller service.
 export const RESOURCE_TYPES = [
   'AWS.ACM.Certificate',
   'AWS.CloudFormation.Stack',
@@ -103,7 +99,6 @@ export const RESOURCE_TYPES = [
   'AWS.Redshift.Cluster',
   'AWS.S3.Bucket',
   'AWS.WAF.Regional.WebACL',
-
   'AWS.WAF.WebACL',
 ] as const;
 
