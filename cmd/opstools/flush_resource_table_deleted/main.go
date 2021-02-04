18c83acb7614712374ad56ca9253f04c123ae4d9
Tate Hanawalt <tate.hanawalt@runpanther.io>
2021-02-04 15:10:02 -0600

    rename and minor cleanup

--- cmd/opstools/flush_resource_table_deleted/main.go
+++ cmd/opstools/flush_resource_table_deleted/main.go
@@ -1,4 +1,24 @@
+// flush_resource_table_deleted removes all items in the panther-resources table which have the
+// field deleted set to true
 package main
+
+/**
+ * Panther is a Cloud-Native SIEM for the Modern Security Team.
+ * Copyright (C) 2020 Panther Labs Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import (
 	"fmt"
@@ -39,19 +59,19 @@ func main() {
 		TableName:                 aws.String(tableName),
 	}
 
-  // Slice of delete item inputs
-	deletes := []*dynamodb.DeleteItemInput{}
-
   // Scan for deleted entries
 	err = client.ScanPages(input, func(page *dynamodb.ScanOutput, lastPage bool) bool {
 		for _, item := range page.Items {
-			input := &dynamodb.DeleteItemInput{
+			fmt.Printf("%v\n", item)
+			_, err := client.DeleteItem(&dynamodb.DeleteItemInput{
 				Key: map[string]*dynamodb.AttributeValue{
 					"id": item["id"],
 				},
 				TableName: aws.String(tableName),
+			})
+			if err != nil {
+				log.Fatal(err)
 			}
-			deletes = append(deletes, input)
 		}
 		return true
 	})
@@ -59,14 +79,5 @@ func main() {
 	if err != nil {
 		fmt.Println("Got error Scanning Pages:")
 		log.Fatal(err)
-	}
-
-  // Loop through deleted items and delete them
-	for index, item := range deletes {
-		fmt.Printf("Index: %v\nitem: %v\n\n", index, item)
-		_, err := client.DeleteItem(item)
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
 }
