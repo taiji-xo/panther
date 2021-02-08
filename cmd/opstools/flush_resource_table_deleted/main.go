fd5278e5582b31cdf0dc305a3ac6217cbf5c91b7
Tate Hanawalt <tate.hanawalt@runpanther.io>
2021-02-08 11:40:58 -0600

    added init and zap logging

--- cmd/opstools/flush_resource_table_deleted/main.go
+++ cmd/opstools/flush_resource_table_deleted/main.go
@@ -21,8 +21,9 @@ package main
  */
 
 import (
-	"log"
 	"time"
+	"os"
+	"log"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -30,6 +31,9 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 
 	"github.com/panther-labs/panther/pkg/awsbatch/dynamodbbatch"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // Explicitely delete columns in the panther-resources table where the table entry deleted is equal
@@ -40,23 +44,49 @@ const entrydeleted = true
 // Max Back Off is used in the request dynamodb batch write items
 const maxBackoff = 10 * time.Second
 
+var awsSession *session.Session
+var sugar *zap.SugaredLogger
+
+func init() {
+	awsSession = session.Must(session.NewSession())
+	logger, err := zap.Config{
+		Encoding: "console",
+		Level: zap.NewAtomicLevelAt(zapcore.DebugLevel),
+		OutputPaths: []string{"stdout"},
+		EncoderConfig: zapcore.EncoderConfig{
+				MessageKey:   "message",
+				// Useful for debugging:
+				// LevelKey:     "level",
+				// EncodeLevel:  zapcore.CapitalLevelEncoder,
+				// TimeKey:      "time",
+				// EncodeTime:   zapcore.ISO8601TimeEncoder,
+				// CallerKey:    "caller",
+				// EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}.Build()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sugar = logger.Sugar()
+}
+
+
 func main() {
-	// AWS Session construction
-	sessOpts := session.Options{SharedConfigState: session.SharedConfigEnable}
-	awsSession := session.Must(session.NewSessionWithOptions(sessOpts))
+
+	sugar.Info("\nFlush Resource Table entries where deleted=true")
+	sugar.Infof("AWS_REGION=%v", *awsSession.Config.Region)
+
 	client := dynamodb.New(awsSession)
 
-	// Query expression
-	proj := expression.NamesList(expression.Name("id"))
-
-	// If you would like to see the value of deleted (or any other field) add it to the projection
-	// names set.
+	// Dynamdb scan expression If you would like to see the value of deleted (or any other field) add
+	// it to the projection names set.
 	// proj := expression.NamesList(expression.Name("id"), expression.Name("deleted"))
-
+	proj := expression.NamesList(expression.Name("id"))
 	filt := expression.Name("deleted").Equal(expression.Value(entrydeleted))
 	expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
 	if err != nil {
-		log.Fatalf("Got error building expression: %s", err)
+		sugar.Error(err)
+		os.Exit(1)
 	}
 
 	// Scan entries with the specified expression above
@@ -69,31 +99,44 @@ func main() {
 		TableName:                 aws.String(tableName),
 	}
 
+	// Slice passed to the batchWriteItem representing the set of all items to remove from the table
 	var deleteRequests []*dynamodb.WriteRequest
 
-	// Scan for deleted entries
-	err = client.ScanPages(input, func(page *dynamodb.ScanOutput, lastPage bool) bool {
+	// Called for each scan response in the ScanPages method - used to build the set of table entries
+	// to delete specified by entry id
+	var scanResult = func(page *dynamodb.ScanOutput, lastPage bool) bool {
 		for _, item := range page.Items {
-			// Useful for troubleshooting:
-			// fmt.Printf("%v\n", item)
-			deleteRequests = append(deleteRequests, &dynamodb.WriteRequest{
-				DeleteRequest: &dynamodb.DeleteRequest{Key: item},
-			})
+			// Delete request for the scan result item
+			deleteEntry := &dynamodb.WriteRequest{DeleteRequest: &dynamodb.DeleteRequest{Key: item}}
+			// Add the delete request to the set
+			deleteRequests = append(deleteRequests, deleteEntry)
 		}
-		return true
-	})
-
-	if err != nil {
-		log.Fatalf("Scan pages error: %s", err)
+		return !lastPage
 	}
 
-	// Batch delete all items
-	err = dynamodbbatch.BatchWriteItem(client, maxBackoff, &dynamodb.BatchWriteItemInput{
+	// Scan for deleted entries
+	if err = client.ScanPages(input, scanResult); err != nil {
+		sugar.Error(err)
+		os.Exit(1)
+	}
+
+	// Exit before calling batch write if no items are found
+	if len(deleteRequests) == 0 {
+		sugar.Info("Resources table scan found no entries where deleted=true\n")
+		os.Exit(0)
+	}
+
+	// Batch write request parameter containing set of delete item requests
+	batchWriteInput := &dynamodb.BatchWriteItemInput{
 		RequestItems: map[string][]*dynamodb.WriteRequest{tableName: deleteRequests},
-	})
-
-	// Batch Write Error
-	if err != nil {
-		log.Fatalf("BatchWriteItem error: %s", err)
 	}
+
+	// Execute the batch deletions
+	if err = dynamodbbatch.BatchWriteItem(client, maxBackoff, batchWriteInput); err != nil {
+		sugar.Errorf("BatchWriteItem error: %s\n", err)
+		os.Exit(1)
+	}
+
+	sugar.Infof("Flushed %v deleted entries\n", len(deleteRequests))
+	os.Exit(0)
 }
