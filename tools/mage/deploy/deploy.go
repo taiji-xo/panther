7373f9c0c531df20760e9209a85ebce36c077184
Austin Byers <austin.byers@runpanther.io>
2021-02-10 15:40:52 -0800

    cloud security configurable python roles

--- tools/mage/deploy/deploy.go
+++ tools/mage/deploy/deploy.go
@@ -484,7 +484,9 @@ func deployCloudSecurityStack(settings *PantherConfig, packager *pkg.Packager, o
 		"LayerVersionArns":           settings.Infra.BaseLayerVersionArns,
 		"ProcessedDataBucket":        outputs["ProcessedDataBucket"],
 		"ProcessedDataTopicArn":      outputs["ProcessedDataTopicArn"],
+		"PythonAssumableRoleArns":    strings.Join(settings.Infra.PythonAssumableRoleArns, ","),
 		"PythonLayerVersionArn":      outputs["PythonLayerVersionArn"],
+		"PythonManagedPolicyArn":     settings.Infra.PythonManagedPolicyArn,
 		"SqsKeyId":                   outputs["QueueEncryptionKeyId"],
 		"TracingMode":                settings.Monitoring.TracingMode,
 
